8cb62d187f56e48b71ba40939e98a890c6c2c118
OxHazem <omarhazemhassan@gmail.com>
2026-01-29 13:13:29 +0200

    Adding the code of the GUI

---
+++ GUI/code/Main_widget.py
@@ -0,0 +1,33 @@
+from PySide6.QtWidgets import QMainWindow , QApplication
+from PySide6.QtUiTools import QUiLoader
+loader = QUiLoader() 
+class Main_widget(QMainWindow):
+    
+    def __init__(self): 
+        super().__init__()
+        self.setWindowTitle("Main Widget")
+        self.setGeometry(100, 100, 800, 600)
+        self.ui = loader.load("GUI/UI/main.ui", self)
+        
+
+        self.arithmetic_widget = loader.load("GUI/UI/Arithmetic.ui", self)
+        # self.geometry_widget = loader.load("GUI/UI/Geometry.ui", self)
+        # self.trig_widget = loader.load("GUI/UI/Trig.ui", self)
+        # self.algebra_widget = loader.load("GUI/UI/Algebra.ui", self)
+        # self.conversions_widget = loader.load("GUI/UI/Conversions.ui", self)
+
+        self.ui.stackedWidget.addWidget(self.arithmetic_widget)
+        # self.ui.stackedWidget.addWidget(self.geometry_widget)
+        # self.ui.stackedWidget.addWidget(self.trig_widget)
+        # self.ui.stackedWidget.addWidget(self.algebra_widget)
+        # self.ui.stackedWidget.addWidget(self.conversions_widget)
+
+
+
+
+        self.ui.Arithmetic.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.arithmetic_widget))
+        self.ui.Geometry.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.geometry_widget))
+        self.ui.Trig.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.trig_widget))
+        self.ui.Algebra.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.algebra_widget))
+        self.ui.Conversions.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.conversions_widget))
+        self.ui.show()
---
+++ GUI/code/main.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QApplication
+from Main_widget import Main_widget
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = Main_widget()
+
+    sys.exit(app.exec())
+
